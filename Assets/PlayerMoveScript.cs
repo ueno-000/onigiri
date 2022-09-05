3c17b7a9fe881e824363b65da994ab1ff594bd51
uen0_ <sarasa4869@zoho.com>
2022-09-05 20:20:27 +0900

    [Update] Playerを転がるようにした

--- Assets/PlayerMoveScript.cs
+++ Assets/PlayerMoveScript.cs
@@ -23,28 +23,19 @@ public class PlayerMoveScript : MonoBehaviour
 
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        _dir = Vector3.forward * v + Vector3.right * h;
-        // カメラのローカル座標系を基準に dir を変換する
-        _dir = Camera.main.transform.TransformDirection(_dir);
-        // カメラは斜め下に向いているので、Y 軸の値を 0 にして「XZ 平面上のベクトル」にする
-        _dir.y = 0;
-        // キャラクターを「現在の（XZ 平面上の）進行方向」に向ける
-        Vector3 forward = _rb.velocity;
-        forward.y = 0;
 
-        if (forward != Vector3.zero)
-        {
-            this.transform.forward = forward;
-        }
+        float dx = Input.GetAxis("Horizontal");
+        float dz = Input.GetAxis("Vertical");
+        var movement = new Vector3(dx, 0, dz);
+        _rb.AddForce(movement * _movePower);
+        
 
         if (isGround == true)//着地しているとき
         {
             if (Input.GetButtonDown("Jump"))
             {
                 isGround = false;//  isGroundをfalseにする
-                _rb.AddForce(transform.up * _jumpPower, ForceMode.Impulse);
+                _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
             }
         }
     }
@@ -58,10 +49,10 @@ public class PlayerMoveScript : MonoBehaviour
     }
 
 
-    void FixedUpdate()
-    {
-        // 「力を加える」処理は力学的処理なので FixedUpdate で行うこと
-        _rb.AddForce(_dir.normalized * _movePower, ForceMode.Force);
-    }
+    //void FixedUpdate()
+    //{
+    //    // 「力を加える」処理は力学的処理なので FixedUpdate で行うこと
+    //    _rb.AddForce(_dir.normalized * _movePower, ForceMode.Acceleration);
+    //}
 
 }
