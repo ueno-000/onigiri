276e424f49efbb4b87a7cca8c960e7335c215adc
uen0_ <sarasa4869@zoho.com>
2022-09-07 23:16:34 +0900

    [Update] 初期Meshの設定

--- Assets/PlayerHPScript.cs
+++ Assets/PlayerHPScript.cs
@@ -8,15 +8,16 @@ public class PlayerHPScript : MonoBehaviour
 {
     /// <summary>�̗͒l</summary>
     [Tooltip("�̗͒l"),SerializeField] private float _hitPoint;
+    /// <summary>Mesh</summary>
+    [Tooltip("Mesh"), SerializeField] private Mesh[] meshes;
 
-    [SerializeField] private Mesh[] meshes;
-    
-
+    MeshFilter _mesh;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+         _mesh = gameObject.transform.GetChild(0).gameObject.GetComponent<MeshFilter>();
+        _mesh.mesh = meshes[0];
     }
 
     // Update is called once per frame
