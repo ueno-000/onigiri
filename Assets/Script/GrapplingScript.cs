9f15ea5e5064cf48c86efb8cc5fa28730f1bfed1
uen0_ <sarasa4869@zoho.com>
2022-09-10 15:56:01 +0900

    [Add] グラップリング用のscript

---
+++ Assets/Script/GrapplingScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingScript : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _target;
+    private Rigidbody _rb;
+    private SpringJoint _sj;
+    private LineRenderer _lineRenderer;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
