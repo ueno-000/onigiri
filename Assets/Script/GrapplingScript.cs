91a52f9bb05ca9e98c7d3a31c5c6f5660a4f2fcc
uen0_ <sarasa4869@zoho.com>
2022-09-26 23:45:45 +0900

    [Update] 不具合修正

--- Assets/Script/GrapplingScript.cs
+++ Assets/Script/GrapplingScript.cs
@@ -23,19 +23,40 @@ public class GrapplingScript : MonoBehaviour
     void Update()
     {
         _sj.connectedBody = _target;
+        _sj.spring = 0;
+        _sj.autoConfigureConnectedAnchor = true;
+        _rb.mass = 10;
 
         _lineRenderer.SetPosition(0, this.gameObject.transform.position);
         _lineRenderer.SetPosition(1,this.gameObject.transform.position);
 
-        if (_target != null)
+        if (_target == null)
         {
             if (Input.GetButtonDown("Fire2"))
             {
-                _target = null;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 20.0f))
+                {
+                    
+                    _target = hit.collider.gameObject.GetComponent<Rigidbody>();
+                }
             }
-
+        }
+        else 
+        {
+            _sj.autoConfigureConnectedAnchor = false;
+            _sj.spring = 5;
+            _sj.connectedAnchor = new Vector3(0, 1, 0);
+            _rb.mass = 1;
             _lineRenderer.SetPosition(0, this.gameObject.transform.position);
             _lineRenderer.SetPosition(1, _target.gameObject.transform.position);
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+                _target = null;
+                
+            }
         }
     }
 }
